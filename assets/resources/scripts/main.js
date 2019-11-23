604e318fb53ec240507452e3ac9769595c89da9a
guilherme <guilhermefribas@outlook.com>
2019-11-23 02:08:24 -0300

    upd

--- assets/resources/scripts/main.js
+++ assets/resources/scripts/main.js
@@ -7,24 +7,18 @@
         return document.getElementById(id);
     };
 
-    function validaPadrao(id, mensagem) {
-        id.addEventListener('invalid', function() {
-            if (id.validity.patternMismatch) {
-                id.setCustomValidity(mensagem);
-            } else {
-                id.setCustomValidity('');
-            }
-        });
-    }
 
     window.addEventListener('load',  function() {
         let inputCpf = $id('input-cpf');
         let inputSenha = $id('input-senha');
 
-        validaPadrao(inputCpf, 
-            'CPF invalido, precisa estar no padrao "000.000.000-00" ou somente numeros!');
-        validaPadrao(inputSenha,
-            'A senha não pode ter acentos, deve ter pelo menos um número e uma letra maiúscula!');
+        inputCpf.addEventListener('invalid', function() {
+            if (inputCpf.validity.patternMismatch) {
+                inputCpf.setCustomValidity('CPF invalido!');
+            } else {
+                inputCpf.setCustomValidity('');
+            }
+        });        
         
         inputCpf.onfocus = function() {
             inputCpf.setAttribute('placeholder', '000.000.000-00');
