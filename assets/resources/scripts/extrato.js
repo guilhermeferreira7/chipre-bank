601cbb688c5b9d56d2bc91748a8cbc9c147b32c6
guilherme <guilhermefribas@outlook.com>
2019-12-07 15:34:04 -0300

    a

--- assets/resources/scripts/extrato.js
+++ assets/resources/scripts/extrato.js
@@ -5,7 +5,7 @@ let thisMovs = accountMov.thisMovs;
 
 if(thisMovs.length > 0)
     for(let mov of thisMovs)
-            document.getElementById('list-extrato').innerHTML += `<li> ${mov} </li>`;
+        document.getElementById('list-extrato').innerHTML += `<li> ${mov} </li>`;
 
 document.getElementById('download-ext').addEventListener('click', function () {
     open('http://localhost:3000/');
